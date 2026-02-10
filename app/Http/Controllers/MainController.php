61aacc7fc62d56317716cd161df5c6fee7742ded
nasteeak <klyachina06@inbox.ru>
2026-02-10 14:18:16 +0500

    one

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public int $a = 1;
+    public int $b = 2;
+
+
+    public function show1()
+    {
+        $a = $this->a;
+        $b = $this->b;
+        $products = ['булочка', 'сок', 'чай', 'кофе', 'вода'];
+        return view('second', compact('a', 'b', 'products'));
+    }
+}
